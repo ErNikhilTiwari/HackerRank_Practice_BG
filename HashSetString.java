514424d464bf2f579ae7f2f6178a96ccba91ceaa
ErNikhilTiwari <102575456+ErNikhilTiwari@users.noreply.github.com>
2025-05-07 13:46:24 +0530

    [Nikhil] ADD HashSet String problem

---
+++ HashSetString.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HashSetString {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+//Write your code here
+     HashSet<String> set=new HashSet<>();
+        for(int i=0;i<t;i++){
+            String s1=pair_left[i]+" "+pair_right[i];
+            String s2=pair_right[i]+" "+pair_left[i];
+
+            if(!set.contains(s2))
+                set.add(s1);
+
+            System.out.println(set.size());
+        }
+
+
+   }
+}
